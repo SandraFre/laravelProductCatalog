11c5903163abafdcde2a7ee083920aca00e42489
SandraFre <SandraFre@users.noreply.github.com>
2020-03-30 17:35:26 +0300

    create.blade

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -5,12 +5,18 @@
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">New Category</div>
+
                 <form action="{{ route('categories.store') }}" method="post">
                     @csrf
                     <div class="card-body">
                         <div class="form-group">
                             <label for="title">Title</label>
-                            <input class="form-control" type="text" name="title" id="title" value="">
+                            <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{old('title')}}">
+                            @error('title')
+                            <div class="alert-danger">
+                                {{$message}}
+                            </div>
+                            @enderror
                         </div>
                     </div>
                     <div class="card-footer">
