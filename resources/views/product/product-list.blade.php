b30ca55f3b885c8ab6019e2c414610f3e0ceaf24
SandraFre <SandraFre@users.noreply.github.com>
2020-03-31 21:44:51 +0300

    changes

--- resources/views/product/product-list.blade.php
+++ resources/views/product/product-list.blade.php
@@ -1,60 +1,63 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-12">
-                <div class="card">
-                    <div class="card-header">
-                        Products
-                        <a href="{{ route('products.create') }}" class="btn btn-sm btn-primary">Add new</a>
-                    </div>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    Products
+                    <a href="{{ route('products.create') }}" class="btn btn-sm btn-primary">Add new</a>
+                </div>
 
-                    <div class="card-body">
-                        <table class="table">
-                            <tr>
-                                <th>#</th>
-                                <th>Title</th>
-                                <th>Slug</th>
-                                <th>Price</th>
-                                <th>Categories</th>
-                                <th>Actions</th>
-                            </tr>
+                <div class="card-body">
+                    <table class="table">
+                        <tr>
+                            <th>#</th>
+                            <th>Title</th>
+                            <th>Slug</th>
+                            <th>Price</th>
+                            <th>Categories</th>
+                            <th>Active</th>
+                            <th>Actions</th>
+                        </tr>
 
-                            @foreach($list as $item)
-                                <tr>
-                                    <td>{{ $item->id }}</td>
-                                    <td>{{ $item->title }}</td>
-                                    <td>{{ $item->slug }}</td>
-                                    <td>{{ $item->price }}</td>
-                                    <td>
-                                        @foreach($item->categories as $category)
-                                            {{$category->title}}<br>
-                                        @endforeach
-                                    </td>
-                                    <td>
-                                        <a href="{{ route('products.edit', ['product' => $item->id]) }}"
-                                           class="btn btn-sm btn-primary">Edit</a>
-                                        <form action="{{ route('products.destroy', ['product' => $item->id]) }}"
-                                              method="post">
-                                            @csrf
-                                            @method('delete')
-                                            <input type="submit"
-                                                   onclick="return confirm('Do you really want to delete a record?');"
-                                                   class="btn btn-sm btn-danger" value="Delete">
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
+                        @foreach($list as $item)
+                        <tr>
+                            <td>{{ $item->id }}</td>
+                            <td>{{ $item->title }}</td>
+                            <td>{{ $item->slug }}</td>
+                            <td>{{ $item->price }}</td>
+                            <td>
+                                @foreach($item->categories as $category)
+                                {{$category->title}}<br>
+                                @endforeach
+                            </td>
+                            <td class="@if ($item->active) text-success @else text-danger @endif">
+                                {{$item->active ? 'Yes': 'No'}}
+                            </td>
+                            <td>
+                                <a href="{{ route('products.edit', ['product' => $item->id]) }}"
+                                    class="btn btn-sm btn-primary">Edit</a>
+                                <form action="{{ route('products.destroy', ['product' => $item->id]) }}" method="post">
+                                    @csrf
+                                    @method('delete')
+                                    <input type="submit"
+                                        onclick="return confirm('Do you really want to delete a record?');"
+                                        class="btn btn-sm btn-danger" value="Delete">
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
 
-                        </table>
-                    </div>
+                    </table>
+                </div>
 
-                    <div class="card-footer">
-                        {{ $list->links() }}
-                    </div>
+                <div class="card-footer">
+                    {{ $list->links() }}
                 </div>
             </div>
         </div>
     </div>
+</div>
 @endsection
