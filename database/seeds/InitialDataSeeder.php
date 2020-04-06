9eaf21a902edb7dd2177c006ac44e0e97b4ea590
SandraFre <SandraFre@users.noreply.github.com>
2020-04-06 19:30:28 +0300

    seed

---
+++ database/seeds/InitialDataSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Seeder;
+
+class InitialDataSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $this->call([
+            CategoriesTableSeeder::class,
+        ]);
+    }
+}
