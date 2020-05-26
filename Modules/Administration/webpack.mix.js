265f0c2bcfcea30760767292cffb53f6dd6c1400
SandraFre <SandraFre@users.noreply.github.com>
2020-05-26 21:48:52 +0300

    administration assets and move layout to module

--- Modules/Administration/webpack.mix.js
+++ Modules/Administration/webpack.mix.js
@@ -1,10 +1,10 @@
-const dotenvExpand = require('dotenv-expand');
-dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
+// const dotenvExpand = require('dotenv-expand');
+// dotenvExpand(require('dotenv').config({ path: '../../.env'/*, debug: true*/}));
 
 const mix = require('laravel-mix');
 require('laravel-mix-merge-manifest');
 
-mix.setPublicPath('../../public').mergeManifest();
+mix.setPublicPath('./Assets').mergeManifest();
 
 mix.js(__dirname + '/Resources/assets/js/app.js', 'js/administration.js')
     .sass( __dirname + '/Resources/assets/sass/app.scss', 'css/administration.css');
